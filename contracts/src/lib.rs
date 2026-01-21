bec857510c86ec7831e2f9e1d179f1cbdba968bd
uche solomon <solomonu928@gmail.com>
2026-01-21 08:41:56 +0100

    feat: Project Initialization

---
+++ contracts/src/lib.rs
@@ -0,0 +1,21 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl};
+
+#[contract]
+pub struct NesteraContract;
+
+// This is a sample contract. Replace this placeholder with your own contract logic.
+// A corresponding test example is available in `test.rs`.
+//
+// For comprehensive examples, visit <https://github.com/stellar/soroban-examples>.
+// The repository includes use cases for the Stellar ecosystem, such as data storage on
+// the blockchain, token swaps, liquidity pools, and more.
+//
+// Refer to the official documentation:
+// <https://developers.stellar.org/docs/build/smart-contracts/overview>.
+#[contractimpl]
+impl NesteraContract {
+
+}
+
+mod test;
